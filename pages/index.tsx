0850366163ad5eba5de6f7fb0da73b0563662714
celestia2222 <newdrago12@gmail.com>
2025-08-08 01:19:43 +0500

    Create index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "4rem", fontFamily: "sans-serif", textAlign: "center" }}>
+      <h1>Welcome to Celestia</h1>
+      <p>🌌 Your astrology site is live. We'll add more magic soon!</p>
+    </main>
+  );
+}
+
